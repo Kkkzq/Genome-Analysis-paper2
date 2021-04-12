d4fb6520c2fdf778b2f2b9cf0448605252ecc79c
Kangzq <kangziqi2000@gmail.com>
2021-04-12 14:01:17 +0200

    codes for dna assembly in 2 ways

--- Scripts/3_SOAPdenovo_wgs.sh
+++ Scripts/3_SOAPdenovo_wgs.sh
@@ -4,16 +4,18 @@
 #SBATCH -M snowy
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 03:00:00
+#SBATCH -t 06:00:00
 #SBATCH -J genome_assembly_soapdenovo
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user ziqi.kang.5312@student.uu.se
 
 # Load modules
 module load bioinfo-tools
+module load soapdenovo
 
 # Your commands
-/sw/bioinfo/SOAPdenovo/2.04-r240/rackham/bin/SOAPdenovo-63mer all \
+/sw/bioinfo/SOAPdenovo/2.04-r240/rackham/bin/SOAPdenovo-127mer all \
 -s /home/ziqi/Ziqi_Genome_Analysis/script/soapdenovo_config.txt \
 -K 49 \
+-R \
 -o /home/ziqi/Ziqi_Genome_Analysis/result/3_dna_assembly/soapdenovo/output
